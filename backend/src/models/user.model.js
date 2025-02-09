41f5b36fb1532ad636ccc3b1966870e587187033
Aditi Solanki <aditisolanki2003@gmail.com>
2025-02-09 22:40:29 +0530

    added user and video models

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -49,39 +49,42 @@ const userShcema = new mongoose.Schema(
   { timestamps: ture }
 );
 
-userShcema.pre("save",async function(next){ // advised not to use arrow funtion as we need to know the context
-  if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password,10)
-    next()
-})
+userShcema.pre("save", async function (next) {
+  // advised not to use arrow funtion as we need to know the context
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
 userShcema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password,this.password)
-}
+  return await bcrypt.compare(password, this.password);
+};
 
-userShcema.methods.generateAccessToken = function(){
-  return jwt.sign({
-    _id: this._id,
-    email:this.email,
-    username:this.username,
-    fullname:this.fullname
-  },
-  process.env.ACCESS_TOKEN_SECRET,
-  {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-  }
-)
-}
+userShcema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
-userShcema.methods.generateRefreshToken = function(){
-  return jwt.sign({
-    _id: this._id,
-  },
-  process.env.REFRESH_TOKEN_SECRET,
-  {
-    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-  }
-)
-}
+userShcema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = new mongoose.model("User", userShcema);
