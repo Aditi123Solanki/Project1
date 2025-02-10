4e3e54617972c2f69d56cd8a2714f1cfd7029b7a
Aditi Solanki <aditisolanki2003@gmail.com>
2025-02-10 10:49:55 +0530

    final project setup completed

---
+++ backend/src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from  "fs"
+
+
+//configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath,{
+            resource_type:auto
+        })
+        // file has been uploaded sucessfully on Cloudinary
+        console.log("file has been uploaded sucessfully on Cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) 
+        // remove the locally save temp file as operation 
+        // got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
